76374cc784b67a106e1999e719be2166c5867bc2
Ian <ian.ledigfr@gmail.com>
2024-12-20 15:04:49 +0100

    adding particle to pressure trap and fixing save issue

--- src/main/java/com/pressuretrap/block/PressureTrapBlock.java
+++ src/main/java/com/pressuretrap/block/PressureTrapBlock.java
@@ -3,11 +3,14 @@ package com.pressuretrap.block;
 import com.pressuretrap.block.tileentity.PressureTrapTileEntity;
 import com.pressuretrap.handler.TileEntityHandler;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PressurePlateBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.particles.BasicParticleType;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.tileentity.TileEntity;
@@ -20,7 +23,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class PressureTrapBlock extends PressurePlateBlock {
 
     public PressureTrapBlock() {
-        super(Sensitivity.EVERYTHING, AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(2.5F).notSolid());
+        super(Sensitivity.EVERYTHING, Properties.create(Material.IRON).hardnessAndResistance(2.5F).notSolid());
     }
 
     @Override
@@ -37,23 +40,45 @@ public class PressureTrapBlock extends PressurePlateBlock {
     public void onEntityCollision(BlockState blockState, World world, BlockPos pos, Entity entity) {
         super.onEntityCollision(blockState, world, pos, entity);
 
-        if (!world.isRemote() && entity instanceof LivingEntity) {
+        if (!world.isRemote) {
+            return;
+        }
+
+        // Apply effect to entity
+        if (entity instanceof LivingEntity) {
             PressureTrapTileEntity tileEntity = (PressureTrapTileEntity) world.getTileEntity(pos);
 
             if (tileEntity != null) {
                 String effectName = tileEntity.getEffect();
-
+                BasicParticleType particleTypes = ParticleTypes.CAMPFIRE_COSY_SMOKE;
+                
                 if (effectName != null && !effectName.equals("Empty")) {
                     if(effectName.equals("Fire")){
+                        particleTypes = ParticleTypes.FLAME;
                         entity.setFire(5);
                     }
                     else {
                         Effect effect = ForgeRegistries.POTIONS.getValue(new ResourceLocation(effectName));
                         if (effect != null) {
+                            particleTypes = ParticleTypes.EFFECT;
                             EffectInstance effectInstance = new EffectInstance(effect, 100, 0);
                             ((LivingEntity) entity).addPotionEffect(effectInstance);
                         }
                     }
+                }
+
+                // Draw particles
+                for (int i = 0; i <= 2; i++) {
+                    double offsetX = world.rand.nextDouble();
+                    double offsetZ = world.rand.nextDouble();
+                    world.addParticle(particleTypes,
+                            pos.getX() + offsetX,
+                            pos.getY() + i,
+                            pos.getZ() + offsetZ,
+                            0.0,
+                            0.02,
+                            0.0
+                    );
                 }
             }
         }
--- src/main/java/com/pressuretrap/block/tileentity/PressureTrapTileEntity.java
+++ src/main/java/com/pressuretrap/block/tileentity/PressureTrapTileEntity.java
@@ -3,6 +3,8 @@ package com.pressuretrap.block.tileentity;
 import com.pressuretrap.handler.TileEntityHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.TileEntity;
 
 public class PressureTrapTileEntity extends TileEntity {
@@ -35,5 +37,25 @@ public class PressureTrapTileEntity extends TileEntity {
         if (compound.contains("Effect")) {
             effect = compound.getString("Effect");
         }
+    }
+
+    @Override
+    public CompoundNBT getUpdateTag() {
+        return this.write(new CompoundNBT());
+    }
+
+    @Override
+    public void handleUpdateTag(BlockState state, CompoundNBT tag) {
+        this.read(state, tag);
+    }
+
+    @Override
+    public SUpdateTileEntityPacket getUpdatePacket() {
+        return new SUpdateTileEntityPacket(this.pos, 1, this.getUpdateTag());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+        this.handleUpdateTag(this.getBlockState(), pkt.getNbtCompound());
     }
 }
